1aecd4fa3540bac131aef39dedb9cbefb2badc4c
qjlxg <qjlxg@163.com>
2025-10-28 20:48:23 +0800

    Update fund_spider.py

--- fund_spider.py
+++ fund_spider.py
@@ -37,7 +37,7 @@ REQUEST_TIMEOUT = 30
 REQUEST_DELAY = 3.5 
 # 优化 1: 增加最大并发数 (原 5)
 MAX_CONCURRENT = 15 
-MAX_FUNDS_PER_RUN = 0 
+MAX_FUNDS_PER_RUN = 5 #限制5个
 PAGE_SIZE = 20
 
 # --------------------------------------------------------------------------------------
